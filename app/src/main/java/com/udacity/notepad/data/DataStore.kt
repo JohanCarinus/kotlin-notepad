b7e1186cc3b84dc49c6c10b2f31c20004a4b4542
Johan Carinus <johan.carinus@entelect.co.za>
2019-10-23 17:54:26 +0200

    Convert all the queries lesson.

--- app/src/main/java/com/udacity/notepad/data/DataStore.kt
+++ app/src/main/java/com/udacity/notepad/data/DataStore.kt
@@ -3,12 +3,7 @@ package com.udacity.notepad.data
 import android.content.Context
 import org.jetbrains.anko.doAsync
 
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-
 object DataStore {
-
-    val EXEC: Executor = Executors.newSingleThreadExecutor()
 
     @JvmStatic
     lateinit var notes: NoteDatabase
@@ -20,5 +15,9 @@ object DataStore {
 
     fun execute(fn: () -> Unit) {
         doAsync { fn() }
+    }
+
+    fun execute(runnable: Runnable) {
+        doAsync { runnable.run() }
     }
 }
